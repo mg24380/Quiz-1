423a21f6db5451e91f36bf2dc62b134568f31b14
mg24380 <mg24380@an098063-ct103v.nvstu.edu>
2015-01-21 17:39:28 -0500

    Miguel Giron Alvarez Quiz 1

---
+++ src/Passwords.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Passwords {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Scanner keyboard = new Scanner(System.in);
+				
+				System.out.println("Enter Your Username");
+				// Set the variables
+				String username = keyboard.next();
+				
+				System.out.println("Enter your password");
+				String password = keyboard.next();
+				
+				
+				System.out.print("Hello " + username + ",Welcome to CSC 200 class! and your password is " + password);
+			
+				
+				
+
+		
+	}
+
+}
